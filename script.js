8564e35c156dec5063b7c11d45e44f42a831d9e6
BEHA-228 <baha63031@gmail.com>
2024-08-24 06:20:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+let son1 = prompt(`1-son kiriting`)
+let son2 = prompt(`2-son kiriting`)
+
+
+function age (a,b) {
+    if (a < b) {
+        alert `${b}`
+    }else if(a > b) {
+        alert `${a}`
+    }
+}
+
+
+
+
+age(3 , 5)   
